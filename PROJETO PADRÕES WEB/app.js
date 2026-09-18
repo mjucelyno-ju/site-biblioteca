c853d0afeee07d4870f82625c9c61b6aad173287
mjucelyno-ju <mjucelyno@gmail.com>
2026-09-18 08:22:50 -0300

    Update app.js
    adicionado codigos.

--- PROJETO PADRÕES WEB/app.js
+++ PROJETO PADRÕES WEB/app.js
@@ -1,1 +1,39 @@
-
+const livros = [
+    {
+        id: 1,
+        titulo: "Fotografia urbana",
+        autor: "j",
+        categoria: "Imagem",
+        capacidade: 30,
+        inscritos: 23,
+        ativa: true
+    },
+    {
+        id: 2,
+        titulo: "Cerâmica manual",
+        autor: "j",
+        categoria: "Artesanato",
+        capacidade: 20,
+        inscritos: 20,
+        ativa: true
+    },
+    {
+        id: 3,
+        titulo: "Desenho de observação",
+        autor: "j",
+        categoria: "Imagem",
+        capacidade: 25,
+        inscritos: 12,
+        ativa: false
+    },
+    {
+        id: 3,
+        titulo: "Desenho de observação",
+        autor: "j",
+        categoria: "Imagem",
+        capacidade: 25,
+        inscritos: 12,
+        ativa: false
+    }
+];
+console.log(livros);
